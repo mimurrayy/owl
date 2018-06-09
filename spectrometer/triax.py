69a84ebe49592fc8002ba086a4f7bd2f8d478000
Julian Held <julian.held@gmail.com>
2018-06-09 10:40:07 +0200

    Triax instrumental function

--- spectrometer/triax.py
+++ spectrometer/triax.py
@@ -10,4 +10,25 @@ class triax(base_spectrometer):
         self.order = order
         self.grating_constant = grating_constant
         self.x = None
-        # self.fine_x = self.make_x_scale(cw,order,64*1024) 
+        # self.fine_x = self.make_x_scale(cw,order,64*1024)
+
+
+    def instrument_function(self, x, xc, transition=None, params = None):
+        if params:
+            w,mu = params
+        elif transition:
+           w,mu = self.get_instrumental_params(transition)
+        y = psd_voigt(x,xc,w,mu)
+        return y
+
+
+    def get_instrumental_params(self, transition):
+        "Database for instrumental functions for lines."
+        if self.order == 1 and self.grating_constant == 50:
+            return (2.48216407e+00, 3.63312977e-01)
+
+
+    # alias
+    instr = instrument_function
+    instrumental_function = instrument_function
+    instrumental_profile = instrument_function
