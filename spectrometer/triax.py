862399aed0f9f7ffe486b7d7680c9898808f1938
Julian Held <julian.held@gmail.com>
2018-10-02 21:12:09 +0200

    Added wavelength calibration for 50line grating of triax spectrometer

--- spectrometer/triax.py
+++ spectrometer/triax.py
@@ -5,12 +5,33 @@ from ..util import *
 from .base_spectrometer import *
 
 class triax(base_spectrometer):
-    def __init__(self, cw, order=1, grating_constant=50):
+    def __init__(self, cw, order=1, grating_constant=50, camera="princeton"):
         self.cw = cw
         self.order = order
         self.grating_constant = grating_constant
-        self.x = None
-        # self.fine_x = self.make_x_scale(cw,order,64*1024)
+        self.camera = camera.lower()
+        self.x = self.make_x_scale(cw,order,1024)
+        self.fine_x = self.make_x_scale(cw,order,64*1024)
+
+
+    def make_x_scale(self, cw=None, order=1, size=977):
+        """Calculates the wavelength scale. Can use bigger sizes to
+        create a better resolution for the plots of the results.
+        Only works for the 50 line/cm grating for now."""
+
+        if self.camera == "princeton":
+            c0 = 5.78
+            c1 = 0.688
+            x = np.linspace(0,size-1,size)
+            x = c0 + cw + (-size/2 + x) * c1
+            return x
+
+        elif self.camera == "andor":
+            c0 = -2.5
+            c1 = 0.345
+            x = np.linspace(0,size-1,size)
+            x = c0 + cw + (-size/2 + x) * c1
+            return x
 
 
     def instrument_function(self, x, xc, transition=None, params = None):
