797fd7ce709d28dc376c593ffe386d60da9e0713
Julian Held <julian.held@gmail.com>
2020-11-10 16:50:34 +0100

    Basic spectrometer is now more flexibel

--- spectrometer/basic.py
+++ spectrometer/basic.py
@@ -4,7 +4,12 @@ import numpy as np
 from ..util import *
 
 class basic():
-    def __init__(self, cw=None, w=None):
+    def __init__(self, cw=None, w=None, wl_range=None):
+        if wl_range:
+            self.wl_range = wl_range
+        else:
+            self.wl_range = 50
+
         if cw:
             self.x = self.make_x_scale(cw)
         else:
@@ -16,6 +21,8 @@ class basic():
             self.w = None
 
 
+
+
     def instrument_function(self, x, xc, transition=None, params = None, w = None):
         dx = np.abs(x[1] - x[0])
         if not w and not self.w:
@@ -25,8 +32,9 @@ class basic():
         y = gauss_function(x,xc,w)
         return y
 
+
     def make_x_scale(self, cw):
-        x = np.linspace(cw-50,cw+50,5000)
+        x = np.linspace(cw-self.wl_range/2, cw+self.wl_range/2, 5000)
         return x
 
 
