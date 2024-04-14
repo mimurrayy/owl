4c4b487c6ccf6b41a53d35af5395b3a2fc30c413
Julian Held <julian.held@gmail.com>
2024-04-13 23:34:47 -0500

    Forgot a required module.

--- setup.py
+++ setup.py
@@ -16,12 +16,12 @@ setuptools.setup(
     url="https://github.com/mimurrayy/owl",
     packages=setuptools.find_packages(),
     install_requires=[
-          'numpy>=1.17.0', 'scipy>=1.10.0', 'mendeleev>=0.12.0', 'astroquery>=0.4.5'
+          'numpy>=1.17.0', 'scipy>=1.10.0', 'mendeleev>=0.12.0', 'astroquery>=0.4.5', 'roman>=4.0'
       ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.5',
+    python_requires='>=3.10',
 )
