5fc0460be3618d703a9c897d6281481df36780b7
Julian Held <julian.held@gmail.com>
2024-04-20 12:23:05 -0500

    Bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="owlspec",
-    version="0.1",
+    version="0.2",
     author="Julian Held",
     author_email="julian.held@umn.edu",
     license='MIT',
