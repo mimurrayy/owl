ed3911290df4a04e29ca39f706c21aa146108f39
Julian Held <julian.held@gmail.com>
2024-04-22 08:46:24 -0500

    Updated list of dependencies.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="owlspec",
-    version="0.2",
+    version="0.2.1",
     author="Julian Held",
     author_email="julian.held@umn.edu",
     license='MIT',
@@ -16,12 +16,12 @@ setuptools.setup(
     url="https://github.com/mimurrayy/owl",
     packages=setuptools.find_packages(),
     install_requires=[
-          'numpy>=1.17.0', 'scipy>=1.10.0', 'mendeleev>=0.12.0', 'astroquery>=0.4.5', 'roman>=4.0'
+          'numpy>=1.23.5', 'scipy>=1.13.0', 'mendeleev>=0.13.0', 'astroquery>=0.4.5', 'roman>=4.0', 'platformdirs>=4.2.0'
       ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.10',
+    python_requires='>=3.10.9',
 )
